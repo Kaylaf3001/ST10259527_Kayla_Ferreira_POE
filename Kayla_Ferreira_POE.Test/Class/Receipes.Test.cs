3141f11c0f7c1cf53efd8fb63a52437fd48294f8
Kayla <Kaylaf3001@gmail.com>
2024-05-30 18:07:17 +0200

    Moved the test folder into the solution

---
+++ Kayla_Ferreira_POE.Test/Class/Receipes.Test.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ST10259527_Kayla_Ferreira_POE.Class;
+using System;
+
+namespace Kayla_Ferreira_POE.Test
+{
+    [TestClass]
+    public class ReceipesTest
+    {
+        // Test the totalCalories method for a null recipe
+        //--------------------------------------------------------------------------------------------------------
+        [TestMethod]
+        public void TestTotalCaloriesForNull()
+        {
+            // Arrange
+            Receipes receipe = new Receipes();
+
+            // Act
+            double totalCalories = receipe.totalCalories();
+
+            // Assert
+            Assert.AreEqual(0, totalCalories);
+        }
+        //--------------------------------------------------------------------------------------------------------
+        // Test the totalCalories method for a recipe over 1000 calories
+        //--------------------------------------------------------------------------------------------------------
+        [TestMethod]
+        public void TestTotalCalorieCheckForFalse()
+        {
+            // Arrange
+            Receipes receipe = new Receipes();
+            // Add some ingredients to the recipe
+            receipe.Ingredients.Add(new Ingredients("Eggs", 1, 1, 100, "Protein"));
+            receipe.Ingredients.Add(new Ingredients("Bread", 2, 2, 200, "Starch"));
+            receipe.Ingredients.Add(new Ingredients("Butter", 1, 1, 100, "Fats"));
+
+            // Act
+            bool overThree = receipe.totalCalorieCheck();
+
+            // Assert
+            Assert.IsFalse(overThree);
+        }
+        //--------------------------------------------------------------------------------------------------------
+        // Test the totalCalories method for a recipe below 300 calories
+        //--------------------------------------------------------------------------------------------------------
+        [TestMethod]
+        public void TestTotalCalorieCheckForHealthyRecipe()
+        {
+            // Arrange
+            Receipes receipe = new Receipes();
+            // Add some ingredients to the recipe
+            receipe.Ingredients.Add(new Ingredients("Eggs", 1, 1, 100, "Protein")); // Low calorie ingredient
+            receipe.Ingredients.Add(new Ingredients("Bread", 2, 2, 100, "Starch")); // Low calorie ingredient
+            receipe.Ingredients.Add(new Ingredients("Butter", 1, 1, 50, "Fats")); // Low calorie ingredient
+
+            // Act
+            bool belowThree = receipe.totalCalorieCheck();
+
+            // Assert
+            Assert.IsTrue(belowThree); // The recipe is healthy because it has low calorie ingredients
+        }
+        //--------------------------------------------------------------------------------------------------------
+    }
+}
---
+++ Kayla_Ferreira_POE.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kayla_Ferreira_POE.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kayla_Ferreira_POE.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("953ecb59-523d-4c4a-afcd-bdb3b249e47e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
